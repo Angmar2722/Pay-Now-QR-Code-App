a2bcf60bf51ba63368fef4855c1113cb53dcb2e1
AJ <anshul@hum22.com>
2020-07-21 11:02:34 +0800

    Initial Commit

---
+++ Small Business QR Code/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Small Business QR Code
+//
+//  Created by Ansh on 21/7/20.
+//  Copyright © 2020 Small Business QR Code. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
